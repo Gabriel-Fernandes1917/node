d3f2602760bf90a3250e44de151d1e649a26a719
Gabriel Fernandes <gabgui2001@gmail.com>
2021-08-20 20:29:56 -0300

    Module in Node.js
    In this class i learned about module.

---
+++ couserYT/class4/calc.js
@@ -0,0 +1,5 @@
+var addFunci = require("./add"); // loading module require("local and name the module")
+var subFunc = require("./sub");
+
+console.log(addFunci(1,2));
+console.log(subFunc(50,3));
---
+++ couserYT/class4/sub.js
@@ -0,0 +1,5 @@
+var sub = function(a,b){
+    return a-b;
+}
+
+module.exports = sub; // form the export one function or value
