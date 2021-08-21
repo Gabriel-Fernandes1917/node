6a005fe220bb7460faea478ec183b68e9a5c8c59
Gabriel Fernandes <gabgui2001@gmail.com>
2021-08-21 12:38:38 -0300

    params

--- Express/index.js
+++ Express/index.js
@@ -15,6 +15,12 @@ app.get("/blog", function(req, res){
     res.send("Welcome my blog");
 })
 
+app.get('/hey/:name/:cargo', function(req, res){ // use '/:' to create params.
+    //res.send(req.params) // req recebe variables.
+    res.send("<h1>hello "+req.params.name+"</h1>"+"<h2>"+req.params.cargo+"</h2>") // to show only one params .name the param.
+    //res.send("<h2>"+req.params+"</h2>"); you can use send only one time.
+})
+
 
 app.listen(8081, function(){
     console.log("Hello World");
